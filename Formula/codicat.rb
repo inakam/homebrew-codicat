f6324a69863f860a8bf6bed1bc425d8a7eb1ecee
homebrew-releaser@example.com <homebrew-releaser@example.com>
2025-04-04 15:45:51 +0000

    chore: brew formula update for codicat v0.1.4

--- Formula/codicat.rb
+++ Formula/codicat.rb
@@ -5,30 +5,30 @@
 class Codicat < Formula
   desc "Display directory tree and file contents from git repositories for llm"
   homepage "https://github.com/inakam/codicat"
-  url "https://github.com/inakam/codicat/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "bb5d43ac3a6cd15e6bd0b4a79996e3c2267f2231d634d37b323a3bb1765cb735"
+  url "https://github.com/inakam/codicat/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "b999dce84de39c4cdc35bdec4c2f75661f00a0ee9231cbfb7016d681176e7c27"
 
   on_macos do
     on_intel do
-      url "https://github.com/inakam/codicat/releases/download/v0.1.3/codicat-0.1.3-darwin-amd64.tar.gz"
-      sha256 "5228bae7f6f03a553e839037ec0462e0d92473b0908d7a1b6e820d8e1b3ea31a"
+      url "https://github.com/inakam/codicat/releases/download/v0.1.4/codicat-0.1.4-darwin-amd64.tar.gz"
+      sha256 "e20d031338f5d855625a8ffebe295cb244b205dcb3d9cb291f49582912ea05e3"
     end
 
     on_arm do
-      url "https://github.com/inakam/codicat/releases/download/v0.1.3/codicat-0.1.3-darwin-arm64.tar.gz"
-      sha256 "ccdf88efcde305492aac2fc535c3686726391676667db6967de0725857745672"
+      url "https://github.com/inakam/codicat/releases/download/v0.1.4/codicat-0.1.4-darwin-arm64.tar.gz"
+      sha256 "c7432c0eda1887e56f99d97c4c959c1a66811695122393a917f078a4005a30cc"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/inakam/codicat/releases/download/v0.1.3/codicat-0.1.3-linux-amd64.tar.gz"
-      sha256 "24ad7b9bc6fe98367504367a734c4c68fe62990e59aca3ff8c7f9089ca9c9613"
+      url "https://github.com/inakam/codicat/releases/download/v0.1.4/codicat-0.1.4-linux-amd64.tar.gz"
+      sha256 "887d96cabaab747cd35a6a95829b59c097a332f33bce5bea0d39dc156859e1cb"
     end
 
     on_arm do
-      url "https://github.com/inakam/codicat/releases/download/v0.1.3/codicat-0.1.3-linux-arm64.tar.gz"
-      sha256 "d75049203b9f8401bbc1d08340fe574d1ddcbdb3697c28cfd79624d06b093cdf"
+      url "https://github.com/inakam/codicat/releases/download/v0.1.4/codicat-0.1.4-linux-arm64.tar.gz"
+      sha256 "20130fd058ebe8aca829ba8c3af1eea3d9c8e69a2f17be375c71c95137d63950"
     end
   end
 
